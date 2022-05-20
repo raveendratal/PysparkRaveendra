20d48a08d1cbfc9a25bdad74378a51ac46e68097
raveendratal <32982143+raveendratal@users.noreply.github.com>
2022-05-20 07:25:08 +0530

    new version

---
+++ notebooks/Shared/retail_project/notebook_clone.py
@@ -0,0 +1,15 @@
+# Databricks notebook source
+# MAGIC %fs ls /
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC show databases
+
+# COMMAND ----------
+
+# MAGIC %fs ls /users/hive/warehouse
+
+# COMMAND ----------
+
+# MAGIC %sh ls

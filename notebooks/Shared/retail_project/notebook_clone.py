c21542e15609a6f12eb3ccff9b0afae03d14f72d
raveendratal <32982143+raveendratal@users.noreply.github.com>
2022-05-20 07:27:21 +0530

    added new dataframe

--- notebooks/Shared/retail_project/notebook_clone.py
+++ notebooks/Shared/retail_project/notebook_clone.py
@@ -13,3 +13,9 @@
 # COMMAND ----------
 
 # MAGIC %sh ls
+
+# COMMAND ----------
+
+# MAGIC %python
+# MAGIC df_emp_csv = spark.read.csv("/FileStore/tables/emp.csv",header=True)
+# MAGIC display(df_emp_csv)

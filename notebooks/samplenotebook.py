22aa7a538fa731b6e513ba4e1e7636e0d51d79aa
TR Raveendra <32982143+raveendratal@users.noreply.github.com>
2022-10-31 20:50:34 +0530

    new version

---
+++ notebooks/samplenotebook.py
@@ -0,0 +1,23 @@
+# Databricks notebook source
+print('this is my first python example')
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC show databases
+
+# COMMAND ----------
+
+display(df) -- df.collect()
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC ls
+
+# COMMAND ----------
+
+# MAGIC %fs ls /
+
+# COMMAND ----------
+

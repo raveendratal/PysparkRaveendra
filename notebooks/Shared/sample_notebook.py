d2f0b2f089f24d125c66262e6c2c6d7abf4e4476
TR Raveendra <32982143+raveendratal@users.noreply.github.com>
2023-04-12 08:23:03 +0530

    new changes

--- notebooks/Shared/sample_notebook.py
+++ notebooks/Shared/sample_notebook.py
@@ -1,2 +1,27 @@
 # Databricks notebook source
-print('this is my first notebook')
+# MAGIC %fs ls /
+
+# COMMAND ----------
+
+# MAGIC %fs ls file:/
+
+# COMMAND ----------
+
+# MAGIC %fs ls dbfs:/FileStore/dept_pipe.csv
+
+# COMMAND ----------
+
+df = spark.read.csv("dbfs:/FileStore/dept_pipe.csv",header=True,inferSchema=True,sep="|")
+display(df)
+
+# COMMAND ----------
+
+df.write.format("delta").mode("overwrite").saveAsTable("dept")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from dept
+
+# COMMAND ----------
+

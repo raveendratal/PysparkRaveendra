4bd2729f5157401a254d471b8207e71ae0db8537
TR Raveendra <32982143+raveendratal@users.noreply.github.com>
2023-06-27 19:50:15 +0530

    new version

---
+++ notebooks/Shared/Untitled Notebook 2023-06-26 20:14:31.py
@@ -0,0 +1,24 @@
+# Databricks notebook source
+# MAGIC %fs ls /
+
+# COMMAND ----------
+
+dbutils.fs.ls("/")
+
+# COMMAND ----------
+
+df1 = spark.read.format("csv").option("header", "true").load("dbfs:/FileStore/tables/emp.csv")
+df2 = spark.read.format("csv").option("header", "true").load("dbfs:/FileStore/tables/dept3.csv")
+
+# COMMAND ----------
+
+df1.write.format("delta").saveAsTable("emp_delta")
+
+# COMMAND ----------
+
+# MAGIC %fs ls dbfs:/FileStore/tables/
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from emp_delta
